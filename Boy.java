f1db336faebdab56b3c4a1dcc7d4b1d8ba3db623
CeceB26 <cestwaila.beck@peopleshores.com>
2026-03-08 11:09:05 -0400

    final

---
+++ Boy.java
@@ -0,0 +1,19 @@
+package GLab303_10_1;
+
+public class Boy extends Person {
+    static double ageFactor = 1.1;
+
+    public Boy() {
+        super();
+    }
+
+    @Override
+    public String talk() {
+        return super.talk() + ".. but I love Java class.";
+    }
+
+    @Override
+    public String walk() {
+        return "I am now walking.";
+    }
+}
